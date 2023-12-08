d4ea55fcc5c9bee96f934f4a7b26d921987eaa6d
ars-yeon <zosel7301@gmail.com>
2023-12-08 13:40:26 +0900

    Enhance torch control and handle camera exceptions

--- app/src/main/java/com/sylee/torchappwidget/torch/TorchManager.kt
+++ app/src/main/java/com/sylee/torchappwidget/torch/TorchManager.kt
@@ -1,4 +1,48 @@
 package com.sylee.torchappwidget.torch
 
-class TorchManager {
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+
+class TorchManager(private val cameraManager: CameraManager) {
+
+    private var cameraId: String? = null
+
+    init {
+        setupCamera()
+    }
+
+    private fun setupCamera() {
+        try {
+            val cameraIds = cameraManager.cameraIdList
+            for (id in cameraIds) {
+                val characteristics = cameraManager.getCameraCharacteristics(id)
+                val hasFlash = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)
+                if (hasFlash == true) {
+                    cameraId = id
+                    return
+                }
+            }
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun turnOnTorch() {
+        toggleTorch(true)
+    }
+
+    fun turnOffTorch() {
+        toggleTorch(false)
+    }
+
+    private fun toggleTorch(enable: Boolean) {
+        try {
+            if (cameraId != null) {
+                cameraManager.setTorchMode(cameraId!!, enable)
+            }
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
 }
